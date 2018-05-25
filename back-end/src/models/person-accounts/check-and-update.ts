54aced5e225e3e8d0ddccf2af5e0b62bbbaf67e5
lunivm <lunivm@yahoo.com>
2018-05-25 15:31:58 +0300

    be: person accounts update on payment submit

--- back-end/src/models/person-accounts/check-and-update.ts
+++ back-end/src/models/person-accounts/check-and-update.ts
@@ -16,16 +16,37 @@ function createPersonAccount(personAccount: PersonAccountInfo) {
 }
 
 function updatePersonAccount(personAccountModel: any, personAccount: PersonAccountInfo) {
-  const financialInstitution = personAccountModel.financialInstitutions.filter((item: any) => {
-    return item.financialInstitution === personAccount.financialInstitution;
+  const fiIndex = personAccountModel.financialInstitutions.findIndex((item: any) => {
+    return item.financialInstitution.toString() === personAccount.financialInstitution;
   });
-  // const count = personAccountModel.financialInstitutions[0].accounts.filter((item: any) => item === personAccount.account).length : -1;
+
+  if (fiIndex > -1) {
+    const accounts = personAccountModel.financialInstitutions[fiIndex].accounts;
+    const accountIndex = accounts.indexOf(personAccount.account);
+
+    if (accountIndex > -1) {
+      // todo: return
+    } else {
+      accounts.push(personAccount.account);
+
+      return personAccountModel.save();
+    }
+  } else {
+    personAccountModel.financialInstitutions.push({
+      financialInstitution: personAccount.financialInstitution,
+      accounts: [
+        personAccount.account
+      ]
+    });
+
+    return personAccountModel.save();
+  }
 }
 
 export function checkAndUpdate(personAccount: PersonAccountInfo) {
   return new Promise((resolve, reject) => {
     if (personAccount.id) {
-      resolve();
+      resolve(); //todo return
     } else {
       delete personAccount.id; // null case
 
@@ -37,20 +58,9 @@ export function checkAndUpdate(personAccount: PersonAccountInfo) {
 
           if (!item) {
             resolve(createPersonAccount(personAccount))
-          } /*else {
+          } else {
             resolve(updatePersonAccount(item, personAccount));
-          }*/
-          // console.log('item', item);
-          // const count = item ?
-          //   item.financialInstitutions[0].accounts.filter((item: any) => item === personAccount.account).length : -1;
-          //
-          // if (count === -1) {
-          //   resolve();
-          // } else if (count === 0) {
-          //
-          // } else {
-          //   resolve(personAccount);
-          // }
+          }
         });
     }
   });
