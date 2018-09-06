a8ea2e56af88c5739e749496cf37d7a02b23db6d
lunivm <lunivm@yahoo.com>
2018-09-06 12:40:54 +0300

    Period reports xls file generation - table layout

--- back-end/src/routes/reports.ts
+++ back-end/src/routes/reports.ts
@@ -1,3 +1,4 @@
+import { BorderStyle } from 'exceljs';
 import express, {
   NextFunction,
   Request,
@@ -27,7 +28,7 @@ router.get('/period', (req: Request, res: Response, next: NextFunction) => {
   PaymentModel
     .find()
     .where('date').gt(startDate).lt(endDate)
-    .exec(function (err: any, result: PaymentModel[]) {
+    .exec(function (err: any, result: any[]) {
       if (err) next(err);
 
       const workbook = new Excel.Workbook();
@@ -36,18 +37,84 @@ router.get('/period', (req: Request, res: Response, next: NextFunction) => {
       res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
       res.setHeader("Content-Disposition", "attachment; filename=" + "Report.xlsx");
 
-      worksheet.columns = [
-        {header: 'Дата платежу', key: 'date', width: 15},
-        {header: 'Призначення платежу', key: 'accountNumber', width: 25},
-        {header: 'Номер рахунку', key: 'description', width: 20}
+      worksheet.mergeCells('A1', 'K1');
+      const captionCell = worksheet.getCell('A1');
+      captionCell.value= `Звіт платежів за період з ${startDate} по ${endDate}`;
+      captionCell.alignment = {
+        vertical: 'middle',
+        horizontal: 'center'
+      };
+      worksheet.getRow(1).height = 50;
+
+      const headerRow = worksheet.getRow(2);
+      headerRow.values = [
+        'Дата виплати',
+        'Номер особового рахунку',
+        'Найменування банку одержувача соціальних виплат',
+        'Код МФО банку одержувача соціальних виплат',
+        'Код згідно ЄДРПОУ банку одержувача соціальних виплат',
+        'Сума соціальних виплат, гривень',
+        'Прізвище, ім\'я, по батькові одержувача соціальних виплат',
+        'Ідентифікаціний код одержувача соціальних виплат',
+        'Серія та номер паспорта одержувача соціальних виплат',
+        'Адреса реєстрації одержувача соціальних виплат',
+        'Призначення платежу'
       ];
 
-      result.forEach((res: PaymentModel) => {
-        worksheet.addRow(({
+      headerRow.height = 110;
+      headerRow.eachCell((cell) => {
+        cell.alignment = {
+          vertical: 'middle',
+          horizontal: 'center',
+          wrapText: true
+        };
+
+        cell.border = {
+          top: {style: 'medium' as BorderStyle},
+          left: {style: 'medium' as BorderStyle},
+          bottom: {style: 'medium' as BorderStyle},
+          right: {style: 'medium' as BorderStyle}
+        }
+      });
+
+      worksheet.columns = [
+        {key: 'date', width: 15},
+        {key: 'account_number', width: 15},
+        {key: 'bank_name', width: 15},
+        {key: 'mfo', width: 15},
+        {key: 'edrpou', width: 15},
+        {key: 'sum', width: 15},
+        {key: 'person', width: 15},
+        {key: 'ident_code', width: 15},
+        {key: 'passport_code', width: 15},
+        {key: 'address', width: 15},
+        {key: 'description', width: 25},
+      ];
+
+      const border = {
+        top: {style: 'thin' as BorderStyle},
+        left: {style: 'thin' as BorderStyle},
+        bottom: {style: 'thin' as BorderStyle},
+        right: {style: 'thin' as BorderStyle}
+      };
+      result.forEach((res) => {
+        const row = worksheet.addRow({
           date: res.date,
-          accountNumber: res.accountNumber,
+          account_number: res.accountNumber,
+          bank_name: res.financialInstitution.name,
+          mfo: res.financialInstitution.mfo,
+          edrpou: res.financialInstitution.edrpou,
+          sum: res.sum,
+          person: res.person.fullName,
+          ident_code: res.person.identityCode,
+          passport_code: res.person.passportNumber,
+          address: res.person.address.street,
           description: res.description
-        }));
+        });
+
+        row.eachCell((cell) => {
+          cell.border = border;
+        });
       });
 
       workbook.xlsx.write(res)
